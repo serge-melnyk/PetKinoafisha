1c1aebbdfe82bdaa4ee08cf95896e9717fbba11b
Serhii V. Melnyk <dozarabotok@gmail.com>
2020-03-02 16:56:33 +0200

    Added sidebar ad component

---
+++ PetKinoafisha01/ClientApp/components/shared/TopSidebarAd.tsx
@@ -0,0 +1,14 @@
+﻿import * as React from "react";
+import ad from "@Images/ads/sidebar-ad2.jpg"
+
+export class TopSidebarAd extends React.Component<{}, {}> {
+    private adSrc = ad;
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return <div className="">
+            <img className="sidebar-header-ad" src={this.adSrc} />
+        </div>;
+    }
+}
--- PetKinoafisha01/ClientApp/pages/HomePage.tsx
+++ PetKinoafisha01/ClientApp/pages/HomePage.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import { RouteComponentProps } from "react-router";
 import { Helmet } from "react-helmet";
 import { BigHeaderAd } from "@Components/shared/BigHeaderAd"
+import { TopSidebarAd } from "@Components/shared/TopSidebarAd";
 import logo from "@Images/logo.png";
 
 type Props = RouteComponentProps<{}>;
@@ -18,17 +19,19 @@ export default class HomePage extends React.Component<Props, {}> {
             </Helmet>
             {/* <img style={{ "margin": "0 auto", "display": "block", "width": "100%" }} src={logo} /> */}
             
-            <div className="panel panel-default">
-                <div className="panel-body row">
-                    <div className="col-md-9">
-                        <div className="container">
+            <div className="container">
+                <div className="row">
+                    <div className="">
+                        <div className="floatLeft">
                             <div className="main-teaser">
                                 <BigHeaderAd></BigHeaderAd>
                             </div>
                         </div>
                     </div>
-                    <div className="col-md-3">
-                        <p>Happy coding</p>
+                    <div className="hidden-md hidden-sm hidden-xs">
+                        <div className="floatLeft">
+                            <TopSidebarAd></TopSidebarAd>
+                        </div>
                     </div>
                 </div>
             </div>
